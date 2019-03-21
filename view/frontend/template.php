4b4f108c741a9a07ebc662094724582477c1010c
Khrull <fcollignon57@gmail.com>
2019-03-21 17:26:33 +0100

    Base du projet

---
+++ view/frontend/template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="description" content="Découvrez le nouveau roman en ligne de Jean Forteroche: 'Billet simple pour l'Alaska'">
+        <meta name="viewport" content="width=device-width">
+        <title><?= $title ?></title>
+        <link href="public/css/style.css" rel="stylesheet" />
+        <link rel="icon" href="public/images/plume.jpg" /> 
+    </head>
+        
+    <body>
+        <?= $content ?>
+    </body>
+</html>
