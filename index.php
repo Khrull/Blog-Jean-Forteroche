deafc5f0b6a14070ffe41f9c6d02c51569cb2a65
Khrull <fcollignon57@gmail.com>
2019-04-09 16:51:36 +0200

    modification de l'index.php

--- index.php
+++ index.php
@@ -1,37 +1,36 @@
 <?php
-require('controller/frontend.php');
 
-try {
-    if (isset($_GET['action'])) {
-        if ($_GET['action'] == 'listPosts') {
-            listPosts();
-        }
-        elseif ($_GET['action'] == 'post') {
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                post();
-            }
-            else {
-                throw new Exception('Aucun identifiant de billet envoyé');
-            }
-        }
-        elseif ($_GET['action'] == 'addComment') {
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-                }
-                else {
-                    throw new Exception('Tous les champs ne sont pas remplis !');
-                }
-            }
-            else {
-                throw new Exception('Aucun identifiant de billet envoyé');
-            }
-        }
-    }
-    else {
-        listPosts();
-    }
+require_once('controller/frontend.php');
+require_once('controller/backend.php');
+
+
+$action ="";
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
 }
+try {
+        switch ($action) {
+
+                case 'login': formLogin();
+                break;
+
+                case 'connexion': login();
+                break;
+
+
+                case 'post': post();
+                break; 
+                
+                case 'listAllPosts': listAllPosts();
+                break;
+
+                case 'addComment': addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                break;
+
+                default: listPosts();    
+            }
+        
+    }
 catch(Exception $e) {
     echo 'Erreur : ' . $e->getMessage();
 }
